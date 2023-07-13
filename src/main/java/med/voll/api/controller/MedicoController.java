4e560820cdda965e40c195e66f796d02ac567d43
paulo <henriquepaulo0606@gmail.com>
2023-07-13 17:32:06 -0300

    Utilizando json pra receber dados do Insomnia

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping ("medicos")
 public class MedicoController {
 
-    @PostMapping
+    @PostMapping //@PostMapping: Determina que o método aceitará requisições HTTP do tipo POST.
+    // @RequestBody: Esta anotação é usada para anotar os argumentos do método do manipulador de solicitações.
     public void cadastrar(@RequestBody String json){ // JSON (JavaScript Object Notation) é um formato utilizado para representação de informações, assim como XML e CSV.
         System.out.println(json);
 
