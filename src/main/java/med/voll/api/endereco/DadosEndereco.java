6f3f2cd8d1b755a792287fc50233a47b0a9f1f56
paulo <henriquepaulo0606@gmail.com>
2023-07-14 18:51:01 -0300

    receber os dados na API. Mas ao invés de recebermos como string, usamos o record. Esse tipo de classe Java ou record chamamos de padrão DTO - Data Transfer Object ("Objeto de transferência de dados").

--- src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -1,4 +1,4 @@
 package med.voll.api.endereco;
-// Classe do tipo record responsavel pelo endereço
+//  Record é um recurso que permite representar uma classe imutável, contendo apenas atributos, construtor e métodos de leitura, de uma maneira muito simples e enxuta.
 public record DadosEndereco(String logradouro, String bairro, String cep, String cidade, String uf, String complemento, String numero) {
 }
