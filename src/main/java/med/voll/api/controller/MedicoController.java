80247f7ca4a93271a309c5e57d5992f8a01c98e1
paulo <henriquepaulo0606@gmail.com>
2023-07-15 17:30:54 -0300

    migração com flyway

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,6 +5,7 @@ import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class MedicoController {
 
     @PostMapping //@PostMapping: Determina que o método aceitará requisições HTTP do tipo POST.
     // @RequestBody indica que um parâmetro de método deve ser associado ao valor do corpo da solicitação HTTP
+    @Transactional // anotação
     public void cadastrar(@RequestBody DadosCadastroMedico dados){ // JSON (JavaScript Object Notation) é um formato utilizado para representação de informações, assim como XML e CSV.
         repository.save(new Medico(dados));
     }
