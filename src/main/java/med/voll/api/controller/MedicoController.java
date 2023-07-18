1665e505c1f3a6b402f5fb35aa96957998396ff6
paulo <henriquepaulo0606@gmail.com>
2023-07-18 12:01:44 -0300

    Atualização de dados  do sistema medico

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,18 +2,13 @@ package med.voll.api.controller;
 
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping ("medicos")
@@ -31,6 +26,13 @@ public class MedicoController {
     @GetMapping // @GetMapping é uma anotação do Spring Framework usada para mapear uma requisição HTTP GET para um método específico em um controlador (controller) de uma aplicação web.
     public Page<DadosListagemMedico> list(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) { // retorno do método list() é uma lista de objetos do tipo DadosListagemMedico. Esse método utiliza o método findAll() do objeto repository para obter todos os registros do banco de dados relacionados à entidade Medico. Esses registros são então convertidos em uma stream (fluxo) e mapeados para objetos DadosListagemMedico por meio do método de referência DadosListagemMedico::new. Finalmente, a stream é coletada em uma lista usando o método toList().
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
+
+    }
+    @PutMapping
+    @Transactional
+    public  void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+       var medico = repository.getReferenceById(dados.id());
+       medico.atualizarInformacoes(dados);
 
     }
 
